f7860c8d9e424a8ff00bcc889e16ba8aeab83dd8
Cegiela, Rafal <Cegiela.Rafal@bcg.com>
2024-11-28 17:26:03 +0400

    Menu own github

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,6 +1,7 @@
 const fetchData = async () => {
     try {
-        const response = await fetch('https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json');
+        const response = await fetch('https://raw.githubusercontent.com/rcegiela/rn_capstone/refs/heads/main/data/menu.json');
+        //'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json'
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
